7e357d72df1a667ab3d144b89a5bd74768a0c38b
Y-DONALD <donaldyoungoua@gmail.com>
2023-11-14 19:27:27 +0100

    fix deployement bug step 2

---
+++ components/SkillCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface CardProps {
+    title: string;
+}
+
+const SkillCard: React.FC<CardProps> = ({ title }) => {
+    return (
+        <div className="mb-4 p-4 h-24 w-4/6 bg-slate-300 rounded-md shadow-md mx-auto">
+            <div className="flex items-center mb-2 justify-center">
+                <h3 className="text-lg font-semibold">{title}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default SkillCard;
