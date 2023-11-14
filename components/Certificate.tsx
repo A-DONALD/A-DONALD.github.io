0a584115cc6bb33979239be0f6271305d14a1c93
Y-DONALD <donaldyoungoua@gmail.com>
2023-11-14 01:34:44 +0100

    finishing project section

--- components/Certificate.tsx
+++ components/Certificate.tsx
@@ -26,13 +26,13 @@ const Certificate: React.FC<CertificateProps> = ({ title, imageSrc, organization
 
     return (
         <div
-            className={`bg-white p-4 mb-4 mx-2 w-80 shadow-md rounded-md cursor-pointer border-4 ${isHovered ? "border-r-slate-600 border-t-slate-600 border-b-orange-600 border-l-orange-600" : "border-white"}`}
+            className={`bg-white p-4 mb-4 mx-2 w-96 shadow-md rounded-md cursor-pointer border-4 ${isHovered ? "border-r-slate-600 border-t-slate-600 border-b-orange-600 border-l-orange-600" : "border-white"}`}
             onClick={handleClick}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
             <div className="flex items-center justify-center mb-2">
-                <img src={imageSrc} alt={title} className=" h-72 w-72 object-cover rounded-md" />
+                <img src={imageSrc} alt={title} className=" w-80 object-cover rounded-md" />
             </div>
             <h3 className="text-xl font-bold mb-2">{title}</h3>
             <p className="text-gray-600 mb-2">{organization}</p>
