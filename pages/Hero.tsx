901f31411ab28cba01115135dedf577afbdf7c7e
Y-DONALD <donaldyoungoua@gmail.com>
2024-02-20 00:24:18 +0100

    add text color

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7  text-center md:text-left py-10 md:py-0">
                     <h1 className="max-w-2xl mb-4 text-gray-600 text-4xl font-extrabold tracking-tight leading-none md:text-6xl lg:text-5xl xl:text-6xl dark:text-white">Donald Youngoua</h1>
-                    <h1 className="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-4xl lg:text-3xl xl:text-4xl dark:text-white">computer engineering student</h1>
+                    <h1 className="max-w-2xl mb-4 text-black text-2xl font-extrabold tracking-tight leading-none md:text-4xl lg:text-3xl xl:text-4xl dark:text-white">computer engineering student</h1>
                     <p className="max-w-2xl mb-6 font-light text-gray-800 lg:mb-8 text-lg md:text-xl dark:text-gray-400">
                         Bienvenue sur mon portfolio! Je suis un étudiant en informatique et réseau passionné
                         par le développement web. Je suis spécialisé en Big Data et mon parcours inclut des projets
